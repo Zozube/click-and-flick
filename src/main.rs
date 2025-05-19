9ab0f18a0be457694ffbbedb7b585c52847cdd95
Anton Tsvil <odenari44@gmail.com>
2025-05-19 20:03:59 +0200

    feat: add bevy crate for random gen

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use bevy::prelude::*;
 use bevy::audio::Volume;
+use bevy::prelude::*;
 use core::time::Duration;
-use rand::seq::IndexedRandom;
+use rand::{RngCore, seq::IndexedRandom};
 
+use bevy_prng::WyRand;
+use bevy_rand::prelude::{EntropyPlugin, GlobalEntropy};
+
+use bevy::dev_tools::picking_debug::{DebugPickingMode, DebugPickingPlugin};
 use bevy_egui::EguiPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy::dev_tools::picking_debug::{DebugPickingMode, DebugPickingPlugin};
 
 #[derive(Resource)]
 struct MyTimer(Timer);
@@ -25,7 +28,7 @@ struct AudioSamples {
 impl Bouncer {
     pub fn bounce(&mut self) {
         self.spd = 5.0;
-    } 
+    }
 
     pub fn update(&mut self, delta: Duration) {
         self.spd += -50.0 * delta.as_secs_f32();
@@ -35,7 +38,6 @@ impl Bouncer {
         }
 
         self.pos += self.spd * delta.as_secs_f32();
-
 
         if self.pos < 0.0 {
             self.pos = 0.0;
@@ -50,13 +52,16 @@ fn main() {
             filter: "bevy_dev_tools=trace".into(), // Show picking logs trace level and up
             ..default()
         }))
+        .add_plugins(EntropyPlugin::<WyRand>::default())
         .add_plugins((MeshPickingPlugin, DebugPickingPlugin))
-        .add_plugins(EguiPlugin { enable_multipass_for_primary_context: true })
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
         .add_plugins(WorldInspectorPlugin::new())
         .insert_resource(MyTimer(Timer::from_seconds(1.0, TimerMode::Once)))
         .insert_resource(MyTimer(Timer::from_seconds(1.0, TimerMode::Once)))
         .add_systems(Startup, (setup, setup_camera, load_punches))
-        .add_systems(Update, (mouse_button_input, update))
+        .add_systems(Update, (mouse_button_input, update, print_random_value))
         .add_systems(
             PreUpdate,
             (|mut mode: ResMut<DebugPickingMode>| {
@@ -79,18 +84,13 @@ fn setup_camera(mut commands: Commands) {
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
-        Sprite::from_image(
-            asset_server.load("private/cave-blue.png"),
-        ),
-        Transform::from_xyz(0., 0., 0.)
+        Sprite::from_image(asset_server.load("private/cave-blue.png")),
+        Transform::from_xyz(0., 0., 0.),
     ));
     commands.spawn(Bouncer::default());
 }
 
-fn load_punches(
-    asset_server: Res<AssetServer>,
-    mut commands: Commands,
-    ) {
+fn load_punches(asset_server: Res<AssetServer>, mut commands: Commands) {
     let samples: Vec<Handle<AudioSource>> = (1..=6)
         .map(|i| asset_server.load(format!("private/non-commercial/punch/{}.ogg", i)))
         .collect();
@@ -133,4 +133,11 @@ fn update(
     let mut transform = transform_q.single_mut().unwrap();
     transform.scale = Vec3::splat(0.4 + bouncer.pos / 10.0);
     bouncer.update(delta);
+}
+
+// Sub-optimal and non-deterministic
+// Use of `GlobalEnthropy` leads to sequenced flow,
+// https://docs.rs/bevy_rand/latest/bevy_rand/tutorial/ch03_components_forking/index.html
+fn print_random_value(mut rng: GlobalEntropy<WyRand>) {
+    println!("Random value {} ", rng.next_u32());
 }
